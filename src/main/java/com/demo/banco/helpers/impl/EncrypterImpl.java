c497e26e71f0eb84a0f17a796c19c58ca6699db1
Julián Guarnieri <102745245+jguarnieri97@users.noreply.github.com>
2023-07-13 19:00:28 -0300

    Agrego librería para hash de password y su funcionalidad

--- src/main/java/com/demo/banco/helpers/impl/EncrypterImpl.java
+++ src/main/java/com/demo/banco/helpers/impl/EncrypterImpl.java
@@ -1,13 +1,18 @@
 package com.demo.banco.helpers.impl;
 
 import com.demo.banco.helpers.Encrypter;
+import com.google.common.hash.Hashing;
 import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class EncrypterImpl implements Encrypter {
 
     @Override
     public String encryptPassword(String pw) {
-        return null;
+        return Hashing.sha256()
+                .hashString(pw, StandardCharsets.UTF_8)
+                .toString();
     }
 }
